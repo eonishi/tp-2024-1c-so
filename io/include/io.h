7ac66424e7789ceee3d68585b00626eb15d10ab9
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-04-19 15:56:46 -0300

    Agrego conexiones de IO/Kernel y IO/Memoria

--- io/include/io.h
+++ io/include/io.h
@@ -4,8 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/log.h>
+#include <commons/config.h>
 #include "../../shared/include/logger.h"
+#include"../../shared/include/client.h"
 
-t_log* logger
+t_log *logger;
+
+typedef struct{
+    char *IP_KERNEL;
+    char *PUERTO_KERNEL;
+    char *IP_MEMORIA;
+    char *PUERTO_MEMORIA;
+    //char *TIPO_INTERFAZ;
+    //unsigned TIEMPO_UNIDAD_TRABAJO;
+    //char *PATH_BASE_DIALFS;
+    //unsigned BLOCK_SIZE;
+    //unsigned BLOCK_COUNT;
+    //unsigned RETRASO_COMPACTACION;
+} io_config;
+
+io_config config;
+
+int crear_conexion_kernel();
 
 #endif
--- io/io.c
+++ io/io.c
@@ -1,16 +1,56 @@
 #include "include/io.h"
 
-int main(){
+int main()
+{
     logger = iniciar_logger("io.log", "IO");
-
     log_info(logger, "Logger IO Iniciado");
+
+    // Esta lógica se repite para cada IO que se quiera crear.
+    inicializar_configuracion();
+    int kernel_fd = crear_conexion_kernel();
+    int memory_fd = crear_conexion_memory();
+    enviar_mensaje(0, kernel_fd);
+    enviar_mensaje(0, memory_fd);
 
     return 0;
 }
 
+int crear_conexion_kernel()
+{
+    log_info(logger, "Creando conexión con CPU...");
+    int conexion = crear_conexion(config.IP_KERNEL, config.PUERTO_KERNEL);
+    log_info(logger, "Conexión creada. Socket: %i", conexion);
+
+    return conexion;
+}
+
+int crear_conexion_memory()
+{
+    log_info(logger, "Creando conexión con CPU...");
+    int conexion = crear_conexion(config.IP_MEMORIA, config.PUERTO_MEMORIA);
+    log_info(logger, "Conexión creada. Socket: %i", conexion);
+
+    return conexion;
+}
+
+void inicializar_configuracion()
+{
+    log_info(logger, "Inicializando configuración...");
+
+    t_config *config_loader = config_create("io.config");
+
+    config.IP_KERNEL = config_get_string_value(config_loader, "IP_KERNEL");
+    config.PUERTO_KERNEL = config_get_string_value(config_loader, "PUERTO_KERNEL");
+
+    config.IP_MEMORIA = config_get_string_value(config_loader, "IP_MEMORIA");
+    config.PUERTO_MEMORIA = config_get_string_value(config_loader, "PUERTO_MEMORIA");
+
+    log_info(logger, "Configuración iniciadada correctamente.");
+}
+
 /*
 Las interfaces de I/O pueden ser varias, en la realidad las conocemos como Teclados, Mouse, Discos, Monitores o hasta Impresoras.
-Las mismas irán recibiendo desde Kernel distintas operaciones a realizar para determinado proceso, 
+Las mismas irán recibiendo desde Kernel distintas operaciones a realizar para determinado proceso,
 y le irán dando aviso a dicho módulo una vez completadas.
 
 
