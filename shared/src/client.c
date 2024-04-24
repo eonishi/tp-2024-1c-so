de3fecb0c1c88d1da35df58061d597d57c55ba08
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-04-24 18:01:59 -0300

    Agrego checks al crear_conexion()

--- shared/src/client.c
+++ shared/src/client.c
@@ -28,20 +28,29 @@ int crear_conexion(char *ip, char* puerto)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(ip, puerto, &hints, &server_info);
+	if(getaddrinfo(ip, puerto, &hints, &server_info) != 0){
+		log_error(logger, "Error en getaddrinfo");
+		return -1;
+	}
+	
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,
+                    			server_info->ai_socktype,
+                    			server_info->ai_protocol);
 
-	socket_cliente = socket(server_info->ai_family,
-                    server_info->ai_socktype,
-                    server_info->ai_protocol);
+	if(socket_cliente == -1){
+		log_error(logger, "Error al crear el socket");
+		return -1;
+	}
 
 	// Ahora que tenemos el socket, vamos a conectarlo
-	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1){
+		log_error(logger, "Error al conectar el socket");
+		return -1;
+	}
 
 	freeaddrinfo(server_info);
-
 	return socket_cliente;
 }
 
