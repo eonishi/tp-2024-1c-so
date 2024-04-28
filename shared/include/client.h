ed59c6dae37e241f81a1eaf3996259a2d44041f5
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-04-28 10:16:48 -0300

    crear_paquete() ahora recibe el codeigo de operacion como parametro

--- shared/include/client.h
+++ shared/include/client.h
@@ -31,7 +31,7 @@ typedef struct
 
 int crear_conexion(char* ip, char* puerto);
 void enviar_mensaje(int codigo_op, char* mensaje, int socket_cliente);
-t_paquete* crear_paquete(void);
+t_paquete* crear_paquete(op_code);
 void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
 void enviar_paquete(t_paquete* paquete, int socket_cliente);
 void liberar_conexion(int socket_cliente);
--- shared/src/client.c
+++ shared/src/client.c
@@ -84,10 +84,10 @@ void crear_buffer(t_paquete* paquete)
 	paquete->buffer->stream = NULL;
 }
 
-t_paquete* crear_paquete(void)
+t_paquete* crear_paquete(op_code cod_op)
 {
 	t_paquete* paquete = malloc(sizeof(t_paquete));
-	paquete->codigo_operacion = PAQUETE;
+	paquete->codigo_operacion = cod_op;
 	crear_buffer(paquete);
 	return paquete;
 }
