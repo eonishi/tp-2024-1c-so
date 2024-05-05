030926027ad31169a24ea4d1b87b9f5a00db5052
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-05 19:59:38 -0300

    Agrego fase de decode, 1° iteracion.

--- cpu/include/ciclo.h
+++ cpu/include/ciclo.h
@@ -2,6 +2,6 @@
 #define CICLO_H_
 
 #include "fetch.h"
-
+#include "decode.h"
 
 #endif
---
+++ cpu/include/decode.h
@@ -0,0 +1,9 @@
+#ifndef DECODE_H_
+#define DECODE_H_
+
+#include <commons/string.h>
+
+char **decode(char *instruccion);
+char **tokenizar_instruccion(char *instruccion);
+
+#endif
