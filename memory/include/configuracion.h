8bdd378eb0feccf721fc306c69a53653c2cdac1c
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-05 22:47:30 -0300

    Refactor de configuracion y conexiones a MEMORIA

---
+++ memory/include/configuracion.h
@@ -0,0 +1,21 @@
+#ifndef CONFIGURACION_MEMORIA_H_
+#define CONFIGURACION_MEMORIA_H_
+
+#include <commons/log.h>
+#include <commons/config.h>
+
+extern t_log* logger;
+
+typedef struct
+{
+    char *ip_memoria;
+    char *puerto_memoria;
+    unsigned tam_memoria;
+    unsigned tam_pagina;
+    char *path_instrucciones;
+    unsigned retardo_memoria;
+} memory_config;
+
+void inicializar_configuracion();
+
+#endif
---
+++ memory/src/conexion.c
@@ -0,0 +1,30 @@
+#include "../include/conexion.h"
+int socket_cpu, socket_kernel;
+
+void esperar_handshake_cpu(int server) {
+    log_info(logger,"Esperando conexión del modulo CPU ... ");
+    socket_cpu = esperar_cliente(server);
+	log_info(logger,"Esperando handshake del modulo CPU ... ");
+    int resultado = esperar_handshake(socket_cpu);
+    if(resultado == -1) {
+        log_error(logger,"No se pudo conectar con el modulo CPU");
+        exit(EXIT_FAILURE);
+    }
+
+    log_info(logger,"Respondiendo handshake del modulo CPU ... ");
+    enviar_handshake(socket_cpu);
+}
+
+void esperar_handshake_kernel(int server) {
+    log_info(logger,"Esperando conexión del modulo Kernel ... ");
+    socket_kernel = esperar_cliente(server);
+	log_info(logger,"Esperando handshake del modulo Kernel ... ");
+    int resultado = esperar_handshake(socket_kernel);
+    if(resultado == -1) {
+        log_error(logger,"No se pudo conectar con el modulo KERNEL");
+        exit(EXIT_FAILURE);
+    }
+
+    log_info(logger,"Respondiendo handshake del modulo Kernel ... ");
+    enviar_handshake(socket_kernel);
+}
