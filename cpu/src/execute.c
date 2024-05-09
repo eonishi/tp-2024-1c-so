e5e39bb80826c0a4d4edf81ef1e50d8272629708
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-08 23:34:29 -0300

    Agrego JNZ y arreglos los bichos de SUM y SUB (son muchos ifff 😶‍🌫️)

--- cpu/src/execute.c
+++ cpu/src/execute.c
@@ -15,15 +15,18 @@ void execute(char **instr_tokenizada)
         exec_sub(instr_tokenizada);
         break;
     case JNZ:
-        /* code */
+        exec_jnz(instr_tokenizada);
         break;
     case IO_GEN_SLEEP:
-        /* code */
+        exec_io_gen_sleep(instr_tokenizada);
         break;
     default:
         log_error(logger, "MAN esta operación no existe (¬_¬\")");
         break;
     }
+
+    free(instr_tokenizada);
+    pcb_actual->pc++;
 }
 
 void exec_set(char **instr_tokenizada){
@@ -43,21 +46,58 @@ void exec_sum(char** instr_tokenizada){
     void* ptr_reg_D = get_registro(instr_tokenizada[1]);
     void* ptr_reg_O = get_registro(instr_tokenizada[2]); 
     if (es8int(instr_tokenizada[1])){
-        *((uint8_t*) ptr_reg_D) += *((unsigned*)ptr_reg_O);
+        if(es8int(instr_tokenizada[2])){
+            *((uint8_t*) ptr_reg_D) += *((uint8_t*)ptr_reg_O);
+        }
+        else{
+            *((uint8_t*) ptr_reg_D) += *((uint32_t*)ptr_reg_O);
+        }
     }
-    else{
-        *((uint32_t*) ptr_reg_D) += *((unsigned*) ptr_reg_O);
+    else if(es8int(instr_tokenizada[2])){
+        *((uint32_t*) ptr_reg_D) += *((uint8_t*) ptr_reg_O);
+    }
+    else {
+        *((uint32_t*) ptr_reg_D) += *((uint32_t*) ptr_reg_O);
     }
 }
 
 void exec_sub(char** instr_tokenizada){
     // SUB reg_D reg_O
     void* ptr_reg_D = get_registro(instr_tokenizada[1]);
-    void* ptr_reg_O = get_registro(instr_tokenizada[2]);
+    void* ptr_reg_O = get_registro(instr_tokenizada[2]); 
     if (es8int(instr_tokenizada[1])){
-        *((uint8_t*) ptr_reg_D) -= *((unsigned*)ptr_reg_O);
+        if(es8int(instr_tokenizada[2])){
+            *((uint8_t*) ptr_reg_D) -= *((uint8_t*)ptr_reg_O);
+        }
+        else{
+            *((uint8_t*) ptr_reg_D) -= *((uint32_t*)ptr_reg_O);
+        }
+    }
+    else if(es8int(instr_tokenizada[2])){
+        *((uint32_t*) ptr_reg_D) -= *((uint8_t*) ptr_reg_O);
+    }
+    else {
+        *((uint32_t*) ptr_reg_D) -= *((uint32_t*) ptr_reg_O);
+    }
+}
+
+void exec_jnz(char** instr_tokenizada){
+    // JNZ registro valor
+    void* ptr_registro = get_registro(instr_tokenizada[1]);
+    int valor = get_valor(instr_tokenizada[2]);
+    if(es8int(instr_tokenizada[1])){ // DIOS demasiados IFFFF
+        if (*((uint8_t*) ptr_registro) != 0){
+            pcb_actual->pc = valor;
+        }
     }
     else{
-        *((uint32_t*) ptr_reg_D) -= *((unsigned*) ptr_reg_O);
+        if (*((uint32_t*) ptr_registro) != 0){
+            pcb_actual->pc = valor;
+        }
     }
+}
+
+void exec_io_gen_sleep(char** instr_tokenizada){
+    // IO_GEN_SLEEP interfaz unidades_trabajo
+    //TODO
 }
