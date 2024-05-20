ad4f5d828c4dfa4f998712fe81493817d1e79def
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-19 21:29:54 -0300

    FIX orden de los parametros

--- cpu/src/checkInterrupt.c
+++ cpu/src/checkInterrupt.c
@@ -4,6 +4,6 @@ void checkInterrupt(){
     if(INTERRUPTION_FLAG){
         INTERRUPTION_FLAG = 0; 
         tengo_pcb = 0; // Envio el pcb a kernel, entonces no tengo más el pcb para ejecutar
-        enviar_pcb(socket_kernel, pcb_actual, INTERRUPCION);
+        enviar_pcb(pcb_actual, socket_kernel, INTERRUPCION);
     }
 }
