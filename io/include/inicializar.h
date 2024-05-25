2a4297edf07e4db17852ab7478888de1431e35bc
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-25 03:54:24 -0300

    Declaracion externa de logger y config

--- io/include/inicializar.h
+++ io/include/inicializar.h
@@ -6,6 +6,9 @@
 #include "../../shared/include/logger.h"
 #include "../../shared/include/io_tipos.h"
 
+extern t_log *logger;
+extern io_config config;
+
 void inicializar_io(char* nombre_interfaz, char* path_config);
 
 #endif
