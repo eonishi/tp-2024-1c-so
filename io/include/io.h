a56592dfdee07cf12b889c6030d2670b61d4669d
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-25 00:30:38 -0300

    Cambio main de IO

--- io/include/io.h
+++ io/include/io.h
@@ -4,34 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <commons/log.h>
-#include <commons/config.h>
-#include "../../shared/include/logger.h"
-#include"../../shared/include/client.h"
-#include"../../shared/include/comunicacion.h"
-#include"../../shared/include/protocolo.h"
+#include "include/inicializar.h"
 
-t_log *logger;
-
-typedef struct{
-    char *IP_KERNEL;
-    char *PUERTO_KERNEL;
-    char *IP_MEMORIA;
-    char *PUERTO_MEMORIA;
-    //char *TIPO_INTERFAZ;
-    //unsigned TIEMPO_UNIDAD_TRABAJO;
-    //char *PATH_BASE_DIALFS;
-    //unsigned BLOCK_SIZE;
-    //unsigned BLOCK_COUNT;
-    //unsigned RETRASO_COMPACTACION;
-} io_config;
-
-io_config config;
-
-int crear_conexion_kernel();
-int crear_conexion_memory();
-
-void inicializar_configuracion();
-void terminar_programa(int conexion_kernel);
+extern t_log *logger;
 
 #endif
--- io/io.c
+++ io/io.c
@@ -1,66 +1,29 @@
 #include "include/io.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    logger = iniciar_logger("io.log", "IO");
-    log_info(logger, "Logger IO Iniciado");
+    char* nombre_interfaz;
+    char* path_config;
 
-    // Esta lógica se repite para cada IO que se quiera crear.
-    inicializar_configuracion();
-    int kernel_fd = crear_conexion_kernel();
-    enviar_mensaje(MENSAJE, "0", kernel_fd);
+    check_args(argc, argv, &nombre_interfaz, &path_config);
+    inicializar_io(nombre_interfaz, path_config);
 
-
-    terminar_programa(kernel_fd);
-
-    // esto a chequear
-    //int memory_fd = crear_conexion_memory();
-    //enviar_mensaje(0, memory_fd);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-int crear_conexion_kernel()
-{
-    log_info(logger, "Creando conexión con CPU...");
-    int conexion = crear_conexion(config.IP_KERNEL, config.PUERTO_KERNEL);
-    log_info(logger, "Conexión creada. Socket: %i", conexion);
-
-    return conexion;
+void check_args(int argc, char* argv[], char** nombre_interfaz, char** path_config){
+    if(argc < 2){
+        log_error(logger, "Falta el nombre de la interfaz");
+        exit(EXIT_FAILURE);
+    }else{
+        *nombre_interfaz = argv[1];
+    }
+    if(argc < 3){
+        *path_config = "io.config";
+    }else{
+        *path_config = argv[2];
+    }
 }
-
-int crear_conexion_memory()
-{
-    log_info(logger, "Creando conexión con CPU...");
-    int conexion = crear_conexion(config.IP_MEMORIA, config.PUERTO_MEMORIA);
-    log_info(logger, "Conexión creada. Socket: %i", conexion);
-
-    return conexion;
-}
-
-void inicializar_configuracion()
-{
-    log_info(logger, "Inicializando configuración...");
-
-    t_config *config_loader = config_create("io.config");
-
-    config.IP_KERNEL = config_get_string_value(config_loader, "IP_KERNEL");
-    config.PUERTO_KERNEL = config_get_string_value(config_loader, "PUERTO_KERNEL");
-
-    config.IP_MEMORIA = config_get_string_value(config_loader, "IP_MEMORIA");
-    config.PUERTO_MEMORIA = config_get_string_value(config_loader, "PUERTO_MEMORIA");
-
-    log_info(logger, "Configuración iniciadada correctamente.");
-}
-
-
-void terminar_programa(int conexion_kernel)
-{
-    liberar_conexion(conexion_kernel);
-    log_info(logger, "Memoria liberada correctamente");
-    log_destroy(logger);
-}
-
 /*
 Las interfaces de I/O pueden ser varias, en la realidad las conocemos como Teclados, Mouse, Discos, Monitores o hasta Impresoras.
 Las mismas irán recibiendo desde Kernel distintas operaciones a realizar para determinado proceso,
