09b2926119e8c58e70825d0d46d686299e515ec8
Dani Nova <dnovacovich@frba.utn.edu.ar>
2024-05-25 20:24:03 +0000

    Refactor gestor_io

--- kernel/src/gestor_io.c
+++ kernel/src/gestor_io.c
@@ -42,12 +42,40 @@ void *escuchar_io(void *socket){
 	}
 }
 
-bool existe_io_conectada(char* nombre_io){
+bool validar_y_enviar_instruccion_a_io(char** instruc_io_tokenizadas){
+    conexion_io* conexion_io;
+
+    if(!existe_io_conectada(instruc_io_tokenizadas[1])){
+        log_error(logger, "La IO no existe: [%s]", instruc_io_tokenizadas[1]);
+        return false;
+    }
+
+    conexion_io = obtener_conexion_io_por_nombre(instruc_io_tokenizadas[1]);
+
+    if(!io_acepta_operacion(*conexion_io, instruc_io_tokenizadas[0])){
+        log_error(logger, "La IO no acepta la operacion: [%s]", instruc_io_tokenizadas[0]);
+        return false;
+    }
+
+    if(!io_disponible(*conexion_io)){
+        log_error(logger, "La IO no está disponible");
+        return false;
+    }
+
+    enviar_instruccion_io(instruc_io_tokenizadas, conexion_io->socket);
+
     return true;
 }
 
+
 conexion_io* obtener_conexion_io_por_nombre(char* nombre_io){
     return (conexion_io*) list_get(lista_conexiones_io, 0);
+}
+
+// Validaciones
+
+bool existe_io_conectada(char* nombre_io){
+    return true;
 }
 
 bool io_acepta_operacion(conexion_io conexion_io,char* operacion_io){
@@ -55,33 +83,5 @@ bool io_acepta_operacion(conexion_io conexion_io,char* operacion_io){
 }
 
 bool io_disponible(conexion_io conexion_io){
-    return true;
-}
-
-
-bool validar_y_enviar_instruccion_a_io(char** instruc_io_tokenizadas){
-    conexion_io* conexion_io;
-
-    if(!existe_io_conectada(instruc_io_tokenizadas[1])){
-        log_error(logger, "La IO no existe: [%s]", instruc_io_tokenizadas[0]);
-
-        return false;
-    }
-
-    log_info(logger, "La io existe");
-    conexion_io = obtener_conexion_io_por_nombre(instruc_io_tokenizadas[1]);
-
-    if(!io_acepta_operacion(*conexion_io, instruc_io_tokenizadas[0])){
-        log_error(logger, "La IO no existe: [%s]", instruc_io_tokenizadas[0]);
-    }
-
-    log_info(logger, "Y la operación es válida.");
-
-    if(!io_disponible(*conexion_io)){
-        log_error(logger, "La IO no está disponible");
-    }
-
-    enviar_instruccion_io(instruc_io_tokenizadas, conexion_io->socket);
-
     return true;
 }
