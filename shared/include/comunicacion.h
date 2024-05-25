7c663196d269706d5ab48d2da5c5143cdd40bed8
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-05-25 00:31:11 -0300

    Agrego enviar Status

--- shared/include/comunicacion.h
+++ shared/include/comunicacion.h
@@ -14,13 +14,15 @@ extern t_log* logger;
 // Envios
 void enviar_handshake(int socket);
 void enviar_mensaje(int codigo_op, char* mensaje, int socket_cliente);
+void enviar_status(int codigo_op, int socket_cliente);
 
 // Esperas
 int esperar_handshake(int socket);
 int esperar_respuesta(int socket, op_code codigo_esperado);
 t_list* recibir_paquete(int);
 void recibir_mensaje(int);
-int recibir_operacion(int);
+int recibir_operacion(int); // Sirve como recibir status
 void* recibir_buffer(int*, int);
+char *recibir_respuesta(int socket);
 
 #endif
