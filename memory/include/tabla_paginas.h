f0770c616a74f4bed503e7538ef76420c372b737
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-06-09 01:07:58 -0300

    Agrego una primera iteracion de las tablas de paginas

--- memory/include/tabla_paginas.h
+++ memory/include/tabla_paginas.h
@@ -2,6 +2,14 @@
 #define TABLAS_DE_PAGINAS_H_
 
 #include <commons/collections/dictionary.h>
+#include <commons/collections/list.h>
+#include <commons/string.h>
+#include "proceso_mem.h"
+#include "frame.h"
+
+typedef t_dictionary t_tabla_paginas;
+
+extern t_list* procesos_en_memoria;
 
 t_dictionary *crear_tabla_paginas();
 
