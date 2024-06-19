1aac2bd7d0b8395c53f00b268b534337a8c8c661
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-06-19 20:53:36 -0300

    Agrego logica de MMU

--- cpu/include/mmu.h
+++ cpu/include/mmu.h
@@ -1,5 +1,18 @@
 #ifndef MMU_H_
 #define MMU_H_
 
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "tlb.h"
+#include "../../shared/include/comunicacion.h"
+#include "../../shared/include/direccion.h"
+#include "../../shared/include/pcb.h"
+#include "../../shared/include/protocolo.h"
+
+extern int socket_memoria;
+extern pcb *pcb_actual;
+
+t_list* mmu(uint32_t direccion_logica, size_t tam_dato, void* dato);
 
 #endif
