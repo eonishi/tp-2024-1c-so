19f2ade4e85ccb10fd2f5ba62d1bf5e1a3f460c0
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-06-19 20:47:34 -0300

    Agrego set_memoria para escribir en memoria y imprimir_mem_hex para hacer memory_dump

--- memory/include/frame.h
+++ memory/include/frame.h
@@ -2,9 +2,11 @@
 #define FRAME_H_
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <commons/collections/list.h>
 #include <commons/collections/dictionary.h>
 #include <commons/bitarray.h>
+#include <commons/memory.h>
 #include <string.h>
 #include "configuracion.h"
 
@@ -12,13 +14,13 @@ extern memory_config config;
 
 void inicializar_memoria();
 void* get_frame(int frame_number, int offset);
-void set_frame(int frame_number, int offset, void *data, size_t size_data);
+void set_memoria(uint32_t direccion_fisica, void *data, size_t size_data);
 bool tengo_espacio_para_agregar(int cantidad_frames);
 void imprimir_frames();
 void imprimir_data(int frame_number, int offset, size_t value_size);
 void marcar_frame_como(unsigned frame_number, int estado);
 unsigned get_available_frame();
 unsigned frames_libres();
-
+void imprimir_memoria_hex();
 
 #endif
