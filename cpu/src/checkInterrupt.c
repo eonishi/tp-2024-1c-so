bcc0e3502579bb8ea16837f1a81acefa1557adb3
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-07-26 16:55:32 -0300

    Fix error de serializacion & simultaneidad de eventos

--- cpu/src/checkInterrupt.c
+++ cpu/src/checkInterrupt.c
@@ -3,9 +3,14 @@
 void checkInterrupt(){
     if(INTERRUPTION_FLAG){
         log_info(logger, "RECIBI UNA INTERRUPCION.");
-        INTERRUPTION_FLAG = 0;
-        tengo_pcb = 0; // Envio el pcb a kernel, entonces no tengo más el pcb para ejecutar, corta el ciclo de cpu.
+            INTERRUPTION_FLAG = 0;
 
-        enviar_pcb(pcb_actual, socket_kernel, MOTIVO_INTERRUPCION);
+        if(tengo_pcb){
+            tengo_pcb = 0; // Envio el pcb a kernel, entonces no tengo más el pcb para ejecutar, corta el ciclo de cpu.
+            enviar_pcb(pcb_actual, socket_kernel, MOTIVO_INTERRUPCION);
+        }
+        else {
+            log_info(logger, "No tengo un pcb para enviar a kernel.");
+        }
     } 
 }
