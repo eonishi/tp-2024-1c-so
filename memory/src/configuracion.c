2425463f3911421f0b314b785423efff12e22f75
Dani Nova <dnovacovich@frba.utn.edu.ar>
2024-08-01 20:48:00 +0000

    Se agrega ip.config a modulo memoria

--- memory/src/configuracion.c
+++ memory/src/configuracion.c
@@ -5,10 +5,12 @@ memory_config config;
 void inicializar_configuracion(){
     log_info(logger, "Inicializando configuración...");
 
-    t_config* config_loader = config_create("memoria.config");
+    t_config* ip_config_loader = config_create("ip.config");
 
-    config.ip_memoria = config_get_string_value(config_loader, "IP_MEMORIA");
-    config.puerto_memoria = config_get_string_value(config_loader, "PUERTO_ESCUCHA");
+    config.ip_memoria = config_get_string_value(ip_config_loader, "IP_MEMORIA");
+    config.puerto_memoria = config_get_string_value(ip_config_loader, "PUERTO_ESCUCHA");
+
+    t_config* config_loader = config_create("memoria.config");
     config.tam_memoria = config_get_int_value(config_loader, "TAM_MEMORIA");
     config.tam_pagina = config_get_int_value(config_loader, "TAM_PAGINA");
     config.path_instrucciones = config_get_string_value(config_loader, "PATH_INSTRUCCIONES");
