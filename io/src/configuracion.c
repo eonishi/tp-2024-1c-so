10d953034ba951ac9a116d125e1104244a8b5af1
Dani Nova <dnovacovich@frba.utn.edu.ar>
2024-08-01 22:19:32 +0000

    Se agrega ip.config en modulo io

--- io/src/configuracion.c
+++ io/src/configuracion.c
@@ -6,6 +6,18 @@ void inicializar_configuracion(char* path_config){
 
     log_info(logger, "Cargando configuración desde archivo: %s", path_config);
 
+    t_config* ip_config_loader = config_create("ip.config");
+
+    if(ip_config_loader == NULL) {
+        log_error(logger, "No se encontro el archivo: [%s]", path_config);
+        exit(EXIT_FAILURE);
+    }
+
+        config.ip_kernel = config_get_string_value(ip_config_loader, "IP_KERNEL");
+    config.puerto_kernel = config_get_string_value(ip_config_loader, "PUERTO_KERNEL");
+    config.ip_memoria = config_get_string_value(ip_config_loader, "IP_MEMORIA");
+    config.puerto_memoria = config_get_string_value(ip_config_loader, "PUERTO_MEMORIA");
+
     t_config* config_loader = config_create(path_config);
 
     if(config_loader == NULL) {
@@ -17,10 +29,6 @@ void inicializar_configuracion(char* path_config){
     // TODO: Archivo de configuracion para cada tipo de interfaz
     config.interfaz_tipo = config_get_string_value(config_loader, "TIPO_INTERFAZ");
     config.unidad_trabajo = config_get_int_value(config_loader, "TIEMPO_UNIDAD_TRABAJO");
-    config.ip_kernel = config_get_string_value(config_loader, "IP_KERNEL");
-    config.puerto_kernel = config_get_string_value(config_loader, "PUERTO_KERNEL");
-    config.ip_memoria = config_get_string_value(config_loader, "IP_MEMORIA");
-    config.puerto_memoria = config_get_string_value(config_loader, "PUERTO_MEMORIA");
     config.path_base_dialfs = config_get_string_value(config_loader, "PATH_BASE_DIALFS");
     config.block_size = config_get_int_value(config_loader, "BLOCK_SIZE");
     config.block_count = config_get_int_value(config_loader, "BLOCK_COUNT");
